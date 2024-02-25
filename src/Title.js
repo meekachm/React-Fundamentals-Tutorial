b4438f9230c0b6ef54857ec490e91067fb519ed0
meekachm <meeka.chm@gmail.com>
2024-02-24 23:39:41 -0800

    header component built

---
+++ src/Title.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div>
+      <h1>SFPOPOS</h1>
+      <div className="Title-Subtitle">
+        <h2>San Franciscos Privately Owned Public Spaces</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Title
